4e2ee9cf8b5d4e9c8660fff8942d42a36d047815
DzianCPP <ocherkiprozaika@gmail.com>
2022-11-20 14:09:30 +0300

    Migrations for the app

--- database/migrations/m0_create_migration_history.php
+++ database/migrations/m0_create_migration_history.php
@@ -1,6 +1,46 @@
 <?php
 
-class m0_create_migration_history
-{
+namespace database\migrations;
 
+use core\application\Database;
+use PDO;
+
+class m0_create_migration_history extends MigrationsBase
+{
+    public function up(): bool
+    {
+        $db = new Database;
+        $conn = $db->getConnection();
+
+        $sqlQuery = "CREATE TABLE migrationHistory(
+                     migrationID int(10) NOT NULL AUTO_INCREMENT,
+                     migrationIndex varchar(10) NOT NULL,
+                     migrationName varchar(100) NOT NULL,
+                     PRIMARY KEY (migrationID))";
+
+        $query = $conn->prepare($sqlQuery);
+
+        if (!$this->trySqlQuery($query, $conn, get_class($this))) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public function down(): bool
+    {
+        $db = new Database();
+        $conn = $db->getConnection();
+        $sqlQuery = "DROP TABLE migrationHistory";
+
+        $query = $conn->prepare($sqlQuery);
+
+        try {
+            $query->execute();
+        } catch (\PDOException $e) {
+            return false;
+        }
+
+        return true;
+    }
 }
--- database/migrations/m1_create_users_table.php
+++ database/migrations/m1_create_users_table.php
@@ -1,6 +1,50 @@
 <?php
 
-class m1_create_users_table
-{
+namespace database\migrations;
 
+use core\application\Database;
+use PDO;
+
+class m1_create_user_table extends MigrationsBase
+{
+    public function up(): bool
+    {
+        $db = new Database;
+        $conn = $db->getConnection();
+
+        $sqlQuery = "CREATE TABLE usersTable(
+                        userID int(20) NOT NULL AUTO_INCREMENT,
+                        email varchar(255) NOT NULL,
+                        fullName varchar(255) default NULL,
+                        password varchar(20) NOT NULL,
+                        login varchar(40) NOT NULL,
+                        PRIMARY KEY(userID))";
+
+        $query = $conn->prepare($sqlQuery);
+
+        if (!$this->trySqlQuery($query, $conn, get_class($this))) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public function down(): bool
+    {
+        $db = new Database();
+        $conn = $db->getConnection();
+
+        $sqlQuery = "DROP TABLE usersTable";
+
+        $query = $conn->prepare($sqlQuery);
+
+        try {
+            $query->execute();
+            $this->migrationHistoryHandler->removeMigrationFromHistory($conn, get_class($this));
+        } catch (\PDOException $e) {
+            return false;
+        }
+
+        return true;
+    }
 }
