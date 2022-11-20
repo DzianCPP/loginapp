26e49ecf95533fcdf7df29e82a76e45f319402c0
DzianCPP <ocherkiprozaika@gmail.com>
2022-11-20 14:06:17 +0300

    File sqlQuesries provides ready-made sql queries for class Users

--- core/models/sqlQueries.php
+++ core/models/sqlQueries.php
@@ -1,6 +1,7 @@
 <?php
 
-class sqlQueries
-{
-
-}
+return [
+    "insertUser" => "INSERT INTO usersTable (email, fullName, gender, status)
+                    VALUES (:email, :fullName, :gender, :status)",
+    "checkEmpty" => "SELECT COUNT(*) FROM usersTable"
+];
