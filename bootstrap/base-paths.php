bb63b9df0ec82f977847cfcd2002246e5d8bbee3
DzianCPP <ocherkiprozaika@gmail.com>
2022-11-20 14:01:04 +0300

    UserController refactoring - removing excess methods

--- bootstrap/base-paths.php
+++ bootstrap/base-paths.php
@@ -1,5 +1,5 @@
 <?php
 
 const BASE_PATH = __DIR__ . '/../';
-const VIEW_PATH = __DIR__ . '/../core/view/';
+const VIEW_PATH = __DIR__ . '../core/views/';
 const CONTROLLERS_PATH = BASE_PATH . 'core/controllers/';
--- core/controllers/UserController.php
+++ core/controllers/UserController.php
@@ -6,7 +6,7 @@ use function MongoDB\BSON\fromJSON;
 
 class UserController
 {
-    public function create(): void
+    public function register(): void
     {
         $users = new Users();
         if ($users->insertNewUser()) {
@@ -20,70 +20,11 @@ class UserController
 
     public function new(string $email = '', string $fullName = ''): void
     {
-        include VIEW_PATH . "/forms/newUser.html";
+        include VIEW_PATH . "forms/register.html";
     }
 
-    public function show(): void
+    public function login(): void
     {
-        $users = new Users();
-        $allUsers = $users->getAllUsers();
-        $this->renderAllUsers($allUsers);
-    }
 
-    public function showById(): void
-    {
-        $users = new Users();
-        $userID = $_GET['userID'];
-        $userID = ltrim(rtrim($userID, '}'), '{');
-        $user = $users->getUserById($userID);
-        $this->renderOneUser($user);
-    }
-
-    public function editUser(): void
-    {
-        $users = new Users();
-        $userID = $_GET['userID'];
-        $userID = rtrim($userID, '}');
-        $userID = ltrim($userID, '{');
-        $userToEdit = $users->getUserById($userID);
-        $currentEmail = $userToEdit['email'];
-        $currentFullName = $userToEdit['fullName'];
-        $currentGender = $userToEdit['gender'];
-        $currentStatus = $userToEdit['status'];
-        include VIEW_PATH . "/forms/editUser.html";
-    }
-
-    public function update(): void
-    {
-        $jsonString = file_get_contents("php://input");
-        $newUserInfo = json_decode($jsonString, true);
-        $users = new Users();
-        if ($users->editUser($newUserInfo)) {
-            http_response_code(200);
-            $this->show();
-        }
-    }
-
-    public function delete(): void
-    {
-        $jsonString = file_get_contents("php://input");
-        $id = json_decode($jsonString, true);
-        $id = $id['userID'];
-        $id = ltrim($id, "\"");
-        $id = rtrim($id, "\"");
-        $users = new Users();
-        if ($users->delete($id)) {
-            http_response_code(200);
-        }
-    }
-
-    private function renderAllUsers(array $allUsers): void
-    {
-        include VIEW_PATH . "tables/users.html";
-    }
-
-    private function renderOneUser(array $user): void
-    {
-        include VIEW_PATH . "tables/one.html";
     }
 }
